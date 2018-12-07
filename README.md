f8a677bbcbd64247f75220374e79310491b31cb1
Jeremiah Fadugba <jeremiah@debmower_ajd>
2018-12-07 09:42:40 +0200

    update ReadMe file

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
 # HIV-1_Progression-Prediction
 A kaggle competition to predict the likelihood that an HIV patient's infection will become less severe, given a small dataset and limited clinical information.
+
+# Getting started
+To be able to repeat the same experiments, you are going to need a laptop with Miniconda (a minimal version of Anaconda) and several Python packages installed which are listed in the requirements.txt file.
+Following instruction would work as is for Mac or Ubuntu linux users, Windows users would need to install and work in the Gitbash terminal.
+
+## Download and install Miniconda
+Please go to the [Anaconda website](https://conda.io/miniconda.html).
+Download and install *the latest* Miniconda version for *Python* 3.6 for your operating system.
+
+```bash
+wget <http:// link to miniconda>
+sh <miniconda .sh>
+```
+
+After that, type:
+
+```bash
+conda --help
+```
+
+and read the manual.
+
+## Create a clone of this repository 
+You can either create a clone or make a download of this repository. To make a clone, simply use the command below on your terminal:
+
+```bash
+git clone https://github.com/jerofad/HIV-1_Progression-Prediction.git
+```
+
+If you are not using git, just download the repository as zip, and unpack it:
+
+```bash
+wget https://github.com/jerofad/HIV-1_Progression-Prediction/archive/master.zip
+#For Mac users:
+#curl -O https://github.com/jerofad/HIV-1_Progression-Prediction/archive/master.zip
+unzip master.zip
+```
+Change into the course folder, then type:
+
+```bash
+#cd HIV-1_Progression-Prediction
+```
+## # To perform Data Analysis
+Run the command below:
+
+```bash
+#python3 data_analysis.py
+```
+The results is saved into the directory named Figures.
+## # To perform Feature Engineering
+Run the command below:
+
+```bash
+#python3 get_features.py
+```
+which saves the new data into the Data Folder.
+## # To Repeat the experiment on all 10 Models
+Run the command below:
+
+```bash
+#python3 models.py
+```
+It saves the result into a text file named "all_models_report.txt"
+## # Final top 5 Models
+Run the command below:
+
+```bash
+#python3 train.py
+```
+the result of the models can be seen in the text file "models_report.txt"
+
--- data_analysis.py
+++ data_analysis.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 
 ##### Library Importation ####
-
+import os
 import seaborn as sns
 import numpy as np
 import scipy
 import pandas as pd
 from matplotlib import pyplot as plt
 
+
+if not os.path.exists('./Figures'):
+    os.mkdir('./Figures')
 #get_ipython().run_line_magic('matplotlib', 'inline')
 #########################################################################
 
@@ -45,7 +48,7 @@ data_X.corr()
 # general data correlation heatmap
 sns.heatmap(data[['Resp','VL-t0', 'CD4-t0']].corr(),cmap='YlGnBu')
 plt.title('Correlation heatmap')
-plt.savefig('corr_matrix_plot')
+plt.savefig('./Figures/corr_matrix_plot')
 
 
 ##############  Histogram of improving '1' of responds after 16 weeks of therapy #################
@@ -54,7 +57,7 @@ data['Resp'].hist()
 #plt.ylabel("nb patient")
 #plt.xlabel("improve 1 or not 0")
 plt.title('Pie Plot of Class 0 and 1: 0 = no improvement')
-plt.savefig('imbalanced_plot')
+plt.savefig('./Figures/imbalanced_plot')
 
 
 
@@ -92,14 +95,14 @@ class_0_X['CD4-t0'].hist()
 plt.ylabel('Effective')
 plt.xlabel('CD4+ count')
 plt.title('Class 0: no improvement')
-plt.savefig('Class0_origin_data_CD4')
+plt.savefig('./Figures/Class0_origin_data_CD4')
 
 ###### VL-t0 histogram
 class_0_X['VL-t0'].hist()
 plt.ylabel('Effective')
 plt.xlabel('viral load')
 plt.title('Class 0: no improvement')
-plt.savefig('Class0_origin_data_VL')
+plt.savefig('./Figures/Class0_origin_data_VL')
 
 
 ############################ Class 1: Improvement ##################################################
@@ -123,7 +126,7 @@ class_1_X['CD4-t0'].hist()
 plt.ylabel('Effective')
 plt.xlabel('CD4+ count')
 plt.title('Class 1: improvement')
-plt.savefig('Class0_origin_data_CD4')
+plt.savefig('./Figures/Class0_origin_data_CD4')
 
 
 ###### VL-t0 histogram
@@ -132,7 +135,7 @@ class_1_X['VL-t0'].hist()
 plt.ylabel('Effective')
 plt.xlabel('viral load')
 plt.title('Class 1: improvement')
-plt.savefig('Class1_origin_data_VL')
+plt.savefig('./Figures/Class1_origin_data_VL')
 
 
 ########################## Plotting  ##########################
@@ -144,7 +147,7 @@ plt.xlabel("nb patient")
 plt.ylabel("viral load")
 
 ######  save figure, format '.png'
-plt.savefig('viral_load_plot.png')
+plt.savefig('./Figures/viral_load_plot.png')
 
 
 ##############  plotting of absolute CD4 count "CD4-t0" #################
